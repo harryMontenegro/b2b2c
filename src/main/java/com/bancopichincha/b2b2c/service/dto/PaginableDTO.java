f87fd8b4a21bd44e9c8f39642d438561e375c192
Harry Montenegro <harryjm018@hotmail.com>
2023-03-29 11:00:50 -0500

    subida8

--- src/main/java/com/bancopichincha/b2b2c/service/dto/PaginableDTO.java
+++ src/main/java/com/bancopichincha/b2b2c/service/dto/PaginableDTO.java
@@ -10,10 +10,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 public class PaginableDTO {
-    @NotNull(message = "Pagina no puede ser null")
-    @Negative(message = "Numero de paginas debe ser mayor a 0")
+
     private Integer pagina;
-    @Negative(message = "Numero de paginas debe ser mayor a 0")
-    @NotNull(message = "Cantidad no puede ser null")
     private Integer cantidad;
 }
