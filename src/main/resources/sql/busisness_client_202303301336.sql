bc2d3a7564644c78ef0158a0e761f507cd42ed37
Harry Montenegro <harryjm018@hotmail.com>
2023-03-31 14:26:19 -0500

    subida17

---
+++ src/main/resources/sql/busisness_client_202303301336.sql
@@ -0,0 +1,51 @@
+INSERT INTO b2b2c.busisness_client (busisness_id,client_id) VALUES
+	 (1,1),
+	 (2,1),
+	 (3,1),
+	 (2,2),
+	 (1,2),
+	 (1,3),
+	 (4,4),
+	 (2,4),
+	 (1,5),
+	 (1,6);
+INSERT INTO b2b2c.busisness_client (busisness_id,client_id) VALUES
+	 (2,6),
+	 (3,6),
+	 (4,6),
+	 (4,7),
+	 (1,7),
+	 (3,8),
+	 (2,8),
+	 (2,9),
+	 (1,9),
+	 (3,9);
+INSERT INTO b2b2c.busisness_client (busisness_id,client_id) VALUES
+	 (3,10),
+	 (4,10),
+	 (1,10),
+	 (1,11),
+	 (4,11),
+	 (2,12),
+	 (3,12),
+	 (4,12),
+	 (4,13),
+	 (1,14);
+INSERT INTO b2b2c.busisness_client (busisness_id,client_id) VALUES
+	 (2,14),
+	 (2,15),
+	 (3,15),
+	 (4,15),
+	 (1,16),
+	 (4,16),
+	 (4,17),
+	 (2,17),
+	 (2,18),
+	 (1,18);
+INSERT INTO b2b2c.busisness_client (busisness_id,client_id) VALUES
+	 (3,18),
+	 (4,18),
+	 (4,19),
+	 (3,19),
+	 (3,20),
+	 (2,20);
---
+++ src/main/resources/sql/client_202303301333.sql
@@ -0,0 +1,22 @@
+INSERT INTO b2b2c.client ("name",lastname,document_type,dni,civil_status,gender,birthdate) VALUES
+	 ('Victor','Pineda',0,'1098652363',1,0,'1985-01-01'),
+	 ('Carmen','Mindiola',0,'10985452212',1,1,'1986-01-01'),
+	 ('Luis','Suares',0,'10236521255',0,0,'1986-01-01'),
+	 ('David','Laya',0,'1023652122',0,0,'1986-01-01'),
+	 ('Manuel','Rojas',0,'1023655212',1,0,'1986-01-01'),
+	 ('Yadira','Fuetes',0,'10236521252',0,1,'1986-01-01'),
+	 ('Deicy','Gamboa',0,'102363225',0,1,'1986-01-01'),
+	 ('Silvia','Isidoro',0,'10254125',0,1,'1986-01-01'),
+	 ('Uriel','Gonzalez',0,'102365125452',1,0,'1986-01-01'),
+	 ('Carolina','Gomez',0,'1023632145',0,1,'1986-01-01');
+INSERT INTO b2b2c.client ("name",lastname,document_type,dni,civil_status,gender,birthdate) VALUES
+	 ('Maria','Lamus',0,'10252125',0,1,'1986-01-01'),
+	 ('Camilo','Sarmiento',0,'12032522',0,0,'1986-01-01'),
+	 ('Marbella','Zalazar',0,'10252142552',0,1,'1986-01-01'),
+	 ('Zara','Valles',0,'10212252',0,1,'1986-01-01'),
+	 ('Carlos','Chirinos',0,'120236522',0,0,'1986-01-01'),
+	 ('Sergio','Mantilla',0,'102365211',0,0,'1986-01-01'),
+	 ('Karen','Garcia',0,'123652123',1,1,'1986-01-01'),
+	 ('Jhon','Poveda',0,'125458744',1,0,'1986-01-01'),
+	 ('Juan','Lozano',0,'10254588',0,0,'1986-01-01'),
+	 ('Julie','Serrano',0,'10256332',1,1,'1986-01-01');
