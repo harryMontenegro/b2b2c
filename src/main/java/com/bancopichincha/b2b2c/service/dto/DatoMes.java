dd22ae9b9654cbf4cde3ae7a9bb0760dffadf49a
Harry Montenegro <harryjm018@hotmail.com>
2023-04-10 17:10:28 -0500

    subida22

---
+++ src/main/java/com/bancopichincha/b2b2c/service/dto/DatoMes.java
@@ -0,0 +1,13 @@
+package com.bancopichincha.b2b2c.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DatoMes {
+    private String mes;
+    private Long total;
+}
