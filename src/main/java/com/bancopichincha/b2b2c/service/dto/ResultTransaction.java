a72291bae65e4e10a67c6a0499ee884b93573571
Harry Montenegro <harryjm018@hotmail.com>
2023-04-11 09:50:34 -0500

    subida24

---
+++ src/main/java/com/bancopichincha/b2b2c/service/dto/ResultTransaction.java
@@ -0,0 +1,19 @@
+package com.bancopichincha.b2b2c.service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class ResultTransaction {
+    private String year;
+    private List<Map<String, Long>> data;
+
+    public ResultTransaction() {
+        this.data = new ArrayList<>();
+    }
+}
