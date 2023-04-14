71b7190d3c4ad08aa3948ddfa1c75cadc55fb6c9
Harry Montenegro <harryjm018@hotmail.com>
2023-04-14 14:24:18 -0500

    subida27

--- src/main/java/com/bancopichincha/b2b2c/domain/TransactionsBusisnessClient.java
+++ src/main/java/com/bancopichincha/b2b2c/domain/TransactionsBusisnessClient.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "transactionsbusisnessclient", schema = "b2b2c")
@@ -14,7 +15,12 @@ public class TransactionsBusisnessClient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer transactionsBusisnessClient_id;
+    @Column(insertable = false,  updatable = false)
     private Integer client_id;
     private Integer busisness_id;
     private LocalDate transactionDate;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "client_id")
+    private Client client;
 }
