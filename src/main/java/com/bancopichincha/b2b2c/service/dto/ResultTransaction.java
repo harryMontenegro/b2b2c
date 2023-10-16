9466ead0f85c8693476326cb36cac9c01db8a533
Harry Montenegro <hjmonten@pichincha.com>
2023-10-16 14:00:37 -0500

    subida39

--- src/main/java/com/bancopichincha/b2b2c/service/dto/ResultTransaction.java
+++ src/main/java/com/bancopichincha/b2b2c/service/dto/ResultTransaction.java
@@ -14,6 +14,7 @@ public class ResultTransaction {
     private List<Map<String, Long>> data;
 
     public ResultTransaction() {
+        System.out.println("year = " + year);
         this.data = new ArrayList<>();
     }
 }
